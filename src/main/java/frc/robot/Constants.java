2ab34a5a1ec3a051a3f892bcdad56d9205bbb4b3
Burrit-o <saemediasae@gmail.com>
2023-02-20 18:24:59 -0700

    2/20 Commit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Axis;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -23,9 +24,16 @@ public final class Constants {
     public static final int StickerMotor = 10;
     public static final int NormalMotor = 20;
     public static final int GripperMotor = 60;
-    public final XboxController m_controller = new XboxController(0);
     public static final DigitalOutput m_intakeswitch = new DigitalOutput(9);
     public static final DigitalOutput m_topgripSwitch = new DigitalOutput(8);
     public static final DigitalOutput m_bottomgripSwitch = new DigitalOutput(7);
  } 
+
+ public static class xboxButtons {
+   public static final XboxController m_controller = new XboxController(0);
+    public static final int A_BUTTON = 0;
+    public static final int B_BUTTON = 1;
+    public static final int X_BUTTON = 2;
+    public static final int Y_BUTTON = 3;
+ }
 }
